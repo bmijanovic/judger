782b5f94b328cc87fae50e14c661507118f88171
Jovan Jokić <102528502+jokicjovan@users.noreply.github.com>
2025-06-22 00:57:20 +0200

    Spring boot project init with dependecies

---
+++ backend/Judger-Server/src/main/java/org/example/judgerserver/JudgerServerApplication.java
@@ -0,0 +1,13 @@
+package org.example.judgerserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JudgerServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JudgerServerApplication.class, args);
+	}
+
+}
---
+++ backend/Judger-Server/src/test/java/org/example/judgerserver/JudgerServerApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.judgerserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JudgerServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
